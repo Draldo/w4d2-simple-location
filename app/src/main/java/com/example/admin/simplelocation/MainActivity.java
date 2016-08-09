87f7116362a1f6068e54269bb890221748e425b2
Draldo <draldo_lopez@hotmail.com>
2016-08-09 17:49:19 -0400

    Get last saved location

--- app/src/main/java/com/example/admin/simplelocation/MainActivity.java
+++ app/src/main/java/com/example/admin/simplelocation/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.admin.simplelocation;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
+    private static final String TAG = MainActivity.class.getSimpleName() + "TAG_";
     private GoogleApiClient mGoogleApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApi = buildGoogleApiClient();
+        mGoogleApi.connect();
     }
 
     protected synchronized GoogleApiClient buildGoogleApiClient() {
@@ -23,5 +32,21 @@ public class MainActivity extends AppCompatActivity implements
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApi);
+        Log.d(TAG, "onConnected: " + location.getLatitude() + " " + location.getLongitude());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
