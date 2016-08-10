a7acfae2a1fdaa199a982dbee089deefb0008b9f
Draldo <draldo_lopez@hotmail.com>
2016-08-09 22:56:42 -0400

    Get last saved location target sdk 23 or more

--- app/src/main/java/com/example/admin/simplelocation/MainActivity.java
+++ app/src/main/java/com/example/admin/simplelocation/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.admin.simplelocation;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
+import java.security.Permission;
+
 public class MainActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName() + "TAG_";
+    private static final int PERMISSION_LOCATION_REQUEST_CODE = 1;
     private GoogleApiClient mGoogleApi;
 
     @Override
@@ -34,10 +41,39 @@ public class MainActivity extends AppCompatActivity implements
                 .build();
     }
 
+
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApi);
-        Log.d(TAG, "onConnected: " + location.getLatitude() + " " + location.getLongitude());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_LOCATION_REQUEST_CODE);
+        }
+        else {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApi);
+            Log.d(TAG, "onConnected: " + location.getLatitude() + " " + location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_LOCATION_REQUEST_CODE: {
+                if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission allowed", Toast.LENGTH_SHORT).show();
+                    Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApi);
+                    Log.d(TAG, "onRequestPermissionsResult: " + location.getLatitude() + " " + location.getLongitude());
+                }
+                else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
